90cde10d54ba643d844678c5a62c067e9fb1e1e8
Mansher Singh <mansherius@gmail.com>
2024-10-29 23:12:38 +0530

    v1 of the main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,20 @@
+import { BackgroundBeams } from "@/components/ui/backgroundBeams";
 import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-			<div className="flex flex-col items-center text-md text-gray-700">
+		<div 
+      className="bg-custom-radial grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen md:p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)] overflow-hidden"
+      style={{
+        background: 
+          "linear-gradient(153deg, rgba(210,160,255,1) 9%, rgba(255,255,255,1) 63%)"
+      }}
+      >
+			<div className="flex flex-col items-center text-md text-gray-700 z-10">
 				This is the developer's application to be used to edit, view and test
 				the Food Knowledge Graph Capstone Project.
 			</div>
-			<main className="flex flex-col gap-8 justify-center md:items-center sm:items-start">
+			<main className="flex flex-col gap-8 justify-center md:items-center sm:items-start z-10">
 				<div className="flex gap-4">
 					<img
 						className="dark:invert"
@@ -22,13 +29,13 @@ export default function Home() {
 				</div>
 
         <p className="font-medium text-md text-wrap py-2 text-gray-600">
-          There are 2 methods to query the Knowledge graph - NLP Queries and placeholder queries.
+          There are 2 methods to query the Knowledge graph - NLP Queries and placeholder queries using forms.
         </p>
 
-				<div className="flex flex-col sm:flex-row gap-8 justify-center items-center">
+				<div className="flex flex-col sm:flex-row gap-6 justify-center items-center">
 					<a
 						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+						href="/dashboard/nlp"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
@@ -36,15 +43,15 @@ export default function Home() {
 					</a>
 					<a
 						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+						href="/dashboard/forms"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						Placeholder Query
+						Forms Query
 					</a>
         </div>
 			</main>
-			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center z-10">
 				<a
 					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
 					href="" // Add the link to the documentation
@@ -76,6 +83,12 @@ export default function Home() {
 					Go to our GitHub →
 				</a>
 			</footer>
+      <div>
+        <p className="text-center text-xs text-gray-500">
+          Devloped by Mansher Singh © 2021 FKG Capstone Project
+        </p>
+      </div>
+      <BackgroundBeams className="z-0" />
 		</div>
 	);
 }
